8ee69ca9e696fcf0376f45d2efbde2d3359a3c59
Nazgul Umirova <nazgul@Nurbeks-MacBook-Pro.local>
2021-10-18 23:07:56 +1100

    Edited add function

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,11 @@
-const createCartHtml = (name, quantity, price, subtotal) => {
-  const html = `  <tr>
+const createCartHtml = (id, name, quantity, price, subtotal) => {
+  const html = `  
+  <tr id="${id}">
 <td>${name}</td>
 <td>${quantity}</td>
 <td>${price}</td>
 <td>${subtotal}</td>
+<td><img class="delete-button" id="bin" src="./images/bin.png" ></td>
 </tr>`;
   return html;
 };
@@ -12,30 +14,50 @@ class Cart {
   constructor() {
     this.items = [];
   }
-  addItem(name, quantity, price, subtotal) {
-    let item = {
-      name: name,
-      quantity: quantity,
-      price: price,
-      subtotal: subtotal,
-    };
-    this.items.push(item);
-    console.log(this.items);
+  addItem(id, name, quantity, price, subtotal) {
+    //if we already have similar item in the cart
+    if (this.items.some((i) => i.id == id)) {
+      let matchingItem = this.items.find((it) => it.id == id);
+      matchingItem.name = name;
+      let a = Number(matchingItem.quantity);
+      let b = Number(quantity);
+      matchingItem.quantity= a+b;
+      (matchingItem.price = price),
+        (matchingItem.subtotal = matchingItem.quantity * matchingItem.price);
+    }
+    // if this item is new
+    else {
+      let item = {
+        id,
+        name: name,
+        quantity: quantity,
+        price: price,
+        subtotal: subtotal,
+      };
+      this.items.push(item);
+    }
+
+    // adding new item
   }
+
   render() {
     let itemsHtmlList = [];
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
       const itemHtml = createCartHtml(
+        item.id,
         item.name,
         item.quantity,
         item.price,
         item.subtotal
       );
       itemsHtmlList.push(itemHtml);
+      console.log(itemsHtmlList);
     }
     const itemsHtml = itemsHtmlList.join("\n");
+    console.log(itemsHtml);
     const itemList = document.getElementById("mycart");
+
     itemList.innerHTML = itemsHtml;
   }
 
@@ -49,4 +71,16 @@ class Cart {
       this.items = JSON.parse(itemsJson);
     }
   }
+
+  deleteItem(itemId) {
+    let newItems = [];
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
+      if (item.id !== itemId) {
+        newItems.push(item);
+      }
+    }
+    this.items = newItems;
+  }
 }
+
--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 const cart = new Cart();
-cart.load();
-cart.render();
-const add1 = document.getElementById("add1");
-const add2 = document.getElementById("add2");
-const add3 = document.getElementById("add3");
 const finalCart = document.getElementById("carttoappear");
 
 function validation() {
@@ -13,31 +8,59 @@ function validation() {
   let quantity1 = document.getElementById("quantity1");
   let quantity2 = document.getElementById("quantity2");
   let quantity3 = document.getElementById("quantity3");
+  let id1 = document.getElementById("id1");
+  let id2 = document.getElementById("id2");
+  let id3 = document.getElementById("id3");
+
   let total = 0;
+
   if (quantity1.value > 0) {
+    const id = 1;
     let n = quantity1.value;
     let price = 669;
     let subtotal = n * price;
-    cart.addItem(name1.innerHTML, quantity1.value, price, subtotal);
-    total += subtotal;
+    cart.addItem(id, name1.innerText, quantity1.value, price, subtotal);
   }
   if (quantity2.value > 0) {
+    let id = 2;
     let n = quantity2.value;
     let price = 669;
     let subtotal = n * 669;
-    cart.addItem(name2.innerHTML, quantity2.value, price, subtotal);
-    total += subtotal;
+    cart.addItem(id, name2.innerText, quantity2.value, price, subtotal);
   }
   if (quantity3.value > 0) {
+    id = 3;
     let n = quantity3.value;
     let price = 1119;
-    let subtotal = n * 669;
-    cart.addItem(name3.innerHTML, quantity3.value, price, subtotal);
-    total += subtotal;
+    let subtotal = n * 1119;
+    cart.addItem(id, name3.innerText, quantity3.value, price, subtotal);
   }
   finalCart.style.visibility = "visible";
+  cart.render();
+  //   cart.save();
+
+  if (quantity1.value > 0) {
+    quantity1.value = "";
+  }
+  if (quantity2.value > 0) {
+    quantity2.value = "";
+  }
+  if (quantity3.value > 0) {
+    quantity3.value = "";
+  }
 }
 
-add1.addEventListener("click", validation);
-add2.addEventListener("click", validation);
-add3.addEventListener("click", validation);
+const itemList = document.getElementById("itemList");
+itemList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("addbutton")) {
+    validation();
+  }
+  if (event.target.classList.contains("delete-button")){
+    let parentItem = event.target.parentElement.parentElement;
+    const itemId = Number(parentItem.dataset.itemName);;
+    cart.deleteItem(itemId);
+    cart.save();
+    cart.render();
+  }
+});
+
